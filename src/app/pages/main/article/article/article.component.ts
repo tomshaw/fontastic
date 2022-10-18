82d57922923a9427fdeb430e75e5057daa053361
Tom Shaw <tomshaw21@gmail.com>
2022-10-18 17:20:45 -0500

    Added font inspector article prose stlye bindings.

--- src/app/pages/main/article/article/article.component.ts
+++ src/app/pages/main/article/article/article.component.ts
@@ -16,6 +16,31 @@ export class ArticleComponent implements OnInit {
   selectedNews: any;
   textContent: any;
 
+  fontScale = [
+    {
+      key: 'prose-sm',
+      value: '0.875rem (14px)'
+    },
+    {
+      key: 'prose-base',
+      value: '1rem (16px)'
+    },
+    {
+      key: 'prose-lg',
+      value: '1.125rem (18px)'
+    },
+    {
+      key: 'prose-xl',
+      value: '1.25rem (20px)'
+    },
+    {
+      key: 'prose-2xl',
+      value: '1.5rem (24px)'
+    }
+  ];
+
+  fontScaleSelected = this.fontScale[4].key;
+
   constructor(
     private newsService: NewsService,
     private messageService: MessageService,
@@ -40,10 +65,16 @@ export class ArticleComponent implements OnInit {
 
   selectedNewsArticle(): void {
     this.selectedNews = this.latestNews[Math.floor(Math.random() * this.latestNews.length)];
+    console.log('selectedNews', this.selectedNews);
   }
 
   updateTextContent(url: string): void {
-    this.messageService.fetchNewsContent(url).then((response: any) => this.textContent = response.textContent);
+    this.messageService.fetchNewsContent(url).then((response: any) => this.textContent = response?.textContent);
+  }
+
+  onSelectFontScale(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.fontScaleSelected = target.value;
   }
 
 }
