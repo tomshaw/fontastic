6e5fd5ee29d40b9d995d06485e36b797b010c603
Tom Shaw <tomshaw21@gmail.com>
2023-11-16 22:12:55 -0600

    Maintenance Updates

--- e2e/main.spec.ts
+++ e2e/main.spec.ts
@@ -1,8 +1,8 @@
 import { BrowserContext, ElectronApplication, Page, _electron as electron } from 'playwright';
 import { test, expect } from '@playwright/test';
-const PATH = require('path');
+import * as PATH from 'path';
 
-test.describe('Check Home Page', async () => {
+test.describe('Check Home Page', () => {
   let app: ElectronApplication;
   let firstWindow: Page;
   let context: BrowserContext;
@@ -46,11 +46,11 @@ test.describe('Check Home Page', async () => {
   //   expect(screenshot).toMatchSnapshot(`home-${browserName}.png`);
   // });
 
-  // test('Check title', async () => {
-  //   const elem = await firstWindow.$('app-home h1');
-  //   const text = await elem.innerText();
-  //   expect(text).toBe('App works !');
-  // });
+  test('Check title', async () => {
+    const elem = await firstWindow.$('app-home h1');
+    const text = elem ? await elem.innerText() : null;
+    expect(text).toBe('App works !');
+  });
 
   test.afterAll( async () => {
     await context.tracing.stop({ path: 'e2e/tracing/trace.zip' });
